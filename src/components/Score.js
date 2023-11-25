6ef309e4256878978f3a4d3f9720552226eea3f7
Yowo <83427860+YowoSK@users.noreply.github.com>
2023-11-26 00:06:50 +0100

    score average calc added color of text

--- src/components/Score.js
+++ src/components/Score.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
 function Score() {
-    return (
-        <div>
-            
-        </div>
-    )
+  const data = [
+    {category: "Problem statement", points: 3},
+    {category: "Scope of the work", points: 6},
+    {category: "Required technology stack", points: 2},
+    {category: "Pricing model", points: 8},
+    {category: "Service level agreements (SLAs)", points: 7},
+    {category: "Selection criteria", points: 5},
+    {category: "Timelines", points: 4},
+    {category: "Contact details", points: 9},
+    {category: "Penalty clauses", points: 1},
+    {category: "Required offer type (binding or non-binding)", points: 10}
+  ];
+
+  const sum = data.reduce((acc, cur) => acc + cur.points, 0);
+  const average = sum / data.length;
+
+  return (
+    <div>
+      <h2 className="text-tmagenta">Average point: {average}</h2>                  
+    </div>
+  )
 }
 
 export default Score

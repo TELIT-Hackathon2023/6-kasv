c5983394595b9f3c1f2b635d81d5e1faadb6aa07
Jan <128244524+nosekJan@users.noreply.github.com>
2023-11-25 20:58:57 +0100

    result

--- src/App.js
+++ src/App.js
@@ -8,15 +8,7 @@ const testData = [
             { name: 'Kritérium 1', score: '90' },
             { name: 'Kritérium 2', score: '85' },
         ],
-    },
-    {
-        title: 'Výsledok 2',
-        summary: 'Toto je zhrnutie výsledku 2.',
-        criteria: [
-            { name: 'Kritérium 3', score: '95' },
-            { name: 'Kritérium 4', score: '80' },
-        ],
-    },
+    }
 ];
 const App = () => {
     return (

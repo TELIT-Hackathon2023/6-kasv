12967a104b0647c90886f6975886b11c182f22a6
Yowo <83427860+YowoSK@users.noreply.github.com>
2023-11-25 15:13:58 +0100

    moved project from our repo to 6-KASV

---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Navigation = () => {
+    return (
+        <nav>
+            <ul>
+
+
+            </ul>
+        </nav>
+    );
+};
+
+export default Navigation;
+
---
+++ src/components/UploadDocuments.js
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+const UploadDocument = () => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    setSelectedFile(file);
+  };
+
+  return (
+    <div>
+      <input type="file" onChange={handleFileChange} />
+      {selectedFile && <p>Selected file: {selectedFile.name}</p>}
+    </div>
+  );
+};
+export default UploadDocument;

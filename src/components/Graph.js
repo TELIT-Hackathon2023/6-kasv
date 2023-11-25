4f8d7e5ad1043ff1010fc5766bfe21d6ad02ce8b
Jan <128244524+nosekJan@users.noreply.github.com>
2023-11-25 23:12:26 +0100

    Update graph

--- src/components/Graph.js
+++ src/components/Graph.js
@@ -1,37 +1,59 @@
-import React from 'react'
-import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory'
+import React from 'react';
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
 
 const data = [
-  {category: "Problem statement", points: 3},
-  {category: "State of the work", points: 6},
-  {category: "Required technology stack", points: 2},
-  {category: "Pricing model", points: 8},
-  {category: "SLAs", points: 7},
-  {category: "Selection criteria", points: 5},
-  {category: "Timelines", points: 4},
-  {category: "Contact details", points: 9},
-  {category: "Penalty clauses", points: 1},
-  {category: "Required offer type (binding or non-binding)", points: 10}
+    { category: "Problem statement", points: 3 },
+    { category: "State of the work", points: 6 },
+    { category: "Req technology stack", points: 2 },
+    { category: "Pricing model", points: 8 },
+    { category: "SLAs", points: 7 },
+    { category: "Selection criteria", points: 5 },
+    { category: "Timelines", points: 4 },
+    { category: "Contact details", points: 9 },
+    { category: "Penalty clauses", points: 1 },
+    { category: "Required offer type (binding or non-binding)", points: 10 }
 ];
 
-function Graph() {
-  return (
-  <div className="w-full h-1/2 screen">
-    <h2>RFP Match</h2>
-    <VictoryChart domainPadding={20} theme={VictoryTheme.material}>
-      <VictoryAxis
-        tickValues={data.map((_, i) => i)}
-        tickFormat={data.map((d) => d.category)}
-      />
-      <VictoryAxis
-        dependentAxis
-        tickFormat={(x) => (`${x} pts`)}
-      />
-      <VictoryBar data={data} x="category" y="points" />
-    </VictoryChart>
-  </div>
-
-  )
+const Graph = () => {
+    return (
+        <div className="flex flex-col items-center max-w-3xl mx-auto border-2 p-4 bg-white rounded-md shadow-md">
+            <h2 className="text-2xl font-bold">RFP Match</h2>
+            <VictoryChart
+                domainPadding={20}
+                height={400}
+                width={800}
+                theme={VictoryTheme.material}
+            >
+                <VictoryAxis
+                    tickValues={data.map((_, i) => i)}
+                    tickFormat={data.map((d) => d.category.split(' '))}
+                    style={{
+                        tickLabels: { fontSize: 12, padding: 5, textAnchor: 'middle' },
+                    }}
+                />
+                <VictoryAxis
+                    dependentAxis
+                    tickFormat={(x) => `${x} pts`}
+                    style={{ tickLabels: { fontSize: 12, padding: 10 } }}
+                />
+                <VictoryBar
+                    data={data}
+                    x="category"
+                    y="points"
+                    style={{
+                        data: {
+                            fill: '#E10075',
+                            width: 20,
+                        },
+                        labels: {
+                            fontSize: 10,
+                            fill: '#333',
+                        },
+                    }}
+                />
+            </VictoryChart>
+        </div>
+    );
 }
 
-export default Graph
+export default Graph;

feded42fe057acb1fac9ccdd4e9f799864003c17
Jan <128244524+nosekJan@users.noreply.github.com>
2023-11-26 10:29:28 +0100

    update style ListOfPage

--- src/components/ListOfPdfs.js
+++ src/components/ListOfPdfs.js
@@ -1,7 +1,22 @@
 import React, { useState, useEffect } from 'react';
 
 const ListOfPdfs = () => {
-    const [pdfs, setPdfs] = useState([]);
+    const [pdfs, setPdfs] = useState([
+        {"filename":"170609_student.pdf","similarity":4.2},
+        {"filename":"2015_RFPWebsiteRedesignRepost.pdf","similarity":4.3},
+        {"filename":"5308.pdf","similarity":4},
+        {"filename":"DRA-0131_2015_08_04_09_40_16_KHV9j_Af8vT_59n8t.pdf","similarity":4},
+        {"filename":"dtits","similarity":4},
+        {"filename":"RequestforInformation_Wastewater_RFP.pdf","similarity":8},
+        {"filename":"RFP 32101-2023-002 Access Control.pdf","similarity":4},
+        {"filename":"RFP 32901-31328-23 Final.pdf","similarity":4},
+        {"filename":"RFP Empanelment_SW_2022.pdf","similarity":4},
+        {"filename":"rfp for software development resources 2023-24 final.pdf","similarity":4.2},
+        {"filename":"RFP2203_Software_Development_Services-Final.pdf","similarity":6.1},
+        {"filename":"RFPDOC_43752.pdf","similarity":3.5999999999999996},
+        {"filename":"UNDPSO-RFP-2015-019.pdf","similarity":2.6}
+    ]);
+
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -34,14 +49,15 @@ const ListOfPdfs = () => {
 
     return (
         <div className="flex justify-center">
-            <h2 className="text-2xl font-bold">List of PDFs </h2>
+            <h2 className="text-2xl font-bold mx-10">List of PDFs : </h2>
             {loading && <p>Loading...</p>}
             {pdfs.length === 0 && !loading ? (
                 <p>No PDFs available</p>
             ) : (
                 <ul>
                     {pdfs.map((pdf, index) => (
-                        <li key={index}>
+                        <li className="border-b cursor-pointer hover:bg-gray-100"
+                            key={index}>
                             <a onClick={handleLoadFile}>
                                 {pdf.filename}
                             </a>
